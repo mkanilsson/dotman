ae2dabb2a004c92630131a20bba60b2005ef1534
Marcus Nilsson <marcus.nilsson@genarp.com>
2023-08-30 17:05:59 +0200

    feat: added sudo support for scripts
    This patch allows sudo/doas support for scripts as stdin, stdout and
    stderr are inherited from the default program.

--- src/script.rs
+++ src/script.rs
@@ -1,33 +1,32 @@
-use std::{path::Path, process::Command};
+use std::{
+    path::Path,
+    process::{Command, Stdio},
+};
 
 use crate::errors::DotManResult;
 
 pub fn run_postinstall(dir: &str) -> DotManResult<()> {
-    let path = Path::new(dir).join(".dotman-postinstall");
-    if !path.exists() {
-        return Ok(());
-    }
-
-    Command::new("/usr/bin/bash")
-        .args([path])
-        .current_dir(dir)
-        .output()?;
-
-    // TODO: Report back if script failed
-    return Ok(());
+    run_script(dir, ".dotman-postinstall")
 }
 
 pub fn run_postupdate(dir: &str) -> DotManResult<()> {
-    let path = Path::new(dir).join(".dotman-postupdate");
+    run_script(dir, ".dotman-postupdate")
+}
+
+fn run_script(dir: &str, script: &str) -> DotManResult<()> {
+    let path = Path::new(dir).join(script);
     if !path.exists() {
         return Ok(());
     }
 
     Command::new("/usr/bin/bash")
-        .args([path])
+        .args(["-xe", path.to_str().unwrap()])
         .current_dir(dir)
+        .stdin(Stdio::inherit())
+        .stdout(Stdio::inherit())
+        .stderr(Stdio::inherit())
         .output()?;
 
     // TODO: Report back if script failed
-    return Ok(());
+    Ok(())
 }
